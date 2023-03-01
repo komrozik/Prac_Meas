81789440f24c7cf7cc851ff0100a4dcfacb4e055
Konstantin Mrozik <konstantin.mrozik@studium.uni-hamburg.de>
2023-03-01 16:13:39 +0100

    added temp sketch

---
+++ arduino_files/temp/temp.ino
@@ -0,0 +1,23 @@
+/************
+ * Author: Konstantin Mrozik
+ * eMail: konstantin.mrozik@studium.uni-hamburg.de 
+ * Date: 2023-03-01
+ * 
+ * This script reads a temperature
+ * ****************/
+
+#include <SD.h>
+#include <SPI.h>
+#include <RTClib.h>
+#include <OneWire.h>
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  
+}
