fd2ea2e27e6e932a6bfd211593be7cda1f09429b
Konstantin Mrozik <konstantin.mrozik@studium.uni-hamburg.de>
2023-03-01 16:23:47 +0100

    Add first module

--- arduino_files/temp/temp.ino
+++ arduino_files/temp/temp.ino
@@ -11,9 +11,23 @@
 #include <RTClib.h>
 #include <OneWire.h>
 
+RTC_DS1307 rtc;
+
+OneWire ow(4);
+
 void setup()
 {
   Serial.begin(9600);
+
+  if(!rtc.begin()){
+    Serial.println("RTC is NOT running. Let's set the time now!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  if(!SD.begin(10)){
+    Serial.println("SD module initialization failed or SD Card is not present!");
+    return;
+  }
 
 }
 
